62a31b3e1cb8d4822926696a3c99e25fa634a71c
Knosper <79541185+Knosper@users.noreply.github.com>
2022-10-25 16:46:03 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 rm -f *.out
-echo "removed old a.out |" && sleep 1
+echo "removed old a.out |"
 gcc -Wall -Wextra -Werror -g microshell.c -o a.out
 echo "----------------- |"
 echo "new a.out created |"
